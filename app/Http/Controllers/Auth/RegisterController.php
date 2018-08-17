c163cff0a9e7f5be6d8158e5246f301a772dd6fc
harveyNorman <wei2215038@gmail.com>
2018-08-17 23:56:26 +1000

    add capture module

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,14 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-        ]);
+            'captcha' => 'required|captcha',
+        ],[
+            'captcha.required' => 'Captcha cannot be empty',
+            'captcha.captcha' => 'Please input correct captcha',
+
+        ]
+
+        );
     }
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,22 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+                        <!-- captcha block -->
+                        <div class="form-group {{ $errors->has('captcha') ? ' has-error' : '' }}">
+                            <label for="captcha" class="col-md-4 control-label">Captcha</label>
+
+                            <div class="col-md-6">
+                                <input id="captcha" class="form-control" name="captcha" >
+
+                                <img class="thumbnail captcha" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="click refresh image">
+
+                                @if ($errors->has('captcha'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('captcha') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
