0de8b98c5cfe0cd2cc27d8940c17068d8b047ffa
harveyNorman <wei2215038@gmail.com>
2018-08-20 23:03:28 +1000

    add avator

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -17,8 +18,19 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user){
-        $user->update($request->all());
-        return redirect()->route('users.show', $user->id)->with('success','Personal Info Update Successfully');
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', 'Personal Info Update Successfully！');
     }
+
+
 }
