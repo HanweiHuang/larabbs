3dea6ee98fcae1542426cec94abf53f265069334
harveyNorman <wei2215038@gmail.com>
2018-08-20 23:41:55 +1000

    add authority function for users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,16 +9,22 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        //auth all function except function show
+        $this->middleware('auth',['except' => ['show']]);
+    }
     //
     public function show(User $user){
         return view('users.show', compact('user'));
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', '403')
+
+@section('content')
+    <div style="text-align: center  ">
+        <h1>403 </h1>
+        {{--<h2>{{$result}}</h2>--}}
+        
+    </div>
+@stop
