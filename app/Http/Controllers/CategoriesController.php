10773062ae0f7f5ea723d51aee653a5a03408150
harveyNorman <wei2215038@gmail.com>
2019-01-19 21:18:05 +1100

    send data to category page

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,9 +6,11 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 
+use App\Models\User;
+
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic, User $user)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         //$topics = Topic::with(['category','user'])->where('category_id', $category->id)->paginate(20);
@@ -18,8 +20,10 @@ class CategoriesController extends Controller
                         ->paginate(20);
 
 
+        $active_users = $user->getActiveUsers();
+
 
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category'));
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
