af5645b8fc2baa05ed9061b42cfe408e70f34be4
harveyNorman <wei2215038@gmail.com>
2019-01-20 22:36:01 +1100

    observer for default upload icon

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,10 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user){
+        if(empty($user->avatar)){
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
