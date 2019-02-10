142827e9b2afebcea78210c29c15c853b4fc676f
harveyNorman <wei2215038@gmail.com>
2019-02-10 17:30:41 +1100

    seperate test to a mysql_test databases

---
+++ database/migrations/single/2019_01_26_125854_create_folders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->text('data')->nullable($value = true);
+            $table->string('visible')->nullable($value = true);
+            $table->string('comment')->default('')->nullable($value = true);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folders');
+    }
+}
